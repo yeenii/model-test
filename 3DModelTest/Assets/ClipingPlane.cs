8cf370e2ccfbdf03e3e258e65a9a25e572176792
yeenii <jye0042@naver.com>
2022-06-06 02:48:15 +0900

    culling Object
    오브젝트 컬링 커밋

---
+++ 3DModelTest/Assets/ClipingPlane.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClipingPlane : MonoBehaviour
+{
+    public Material material;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Plane plane = new Plane(transform.up, transform.position);
+
+        Vector4 planeVisulization = new Vector4(plane.normal.x, plane.normal.y, plane.normal.z, plane.distance);
+        material.SetVector("_Plane", planeVisulization);
+    }
+}
